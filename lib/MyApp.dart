ca66e5450b56171b0428f7e33f3a30e5a2b59231
Mandeep <71759045+Mandeep-vivu@users.noreply.github.com>
2023-04-10 12:00:13 +0530

    addingposter

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -15,11 +15,35 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String selectedDate = "29 April"; //default selected date
   bool _isPressed = false;
-  bool _isPressed1 = false;
+  bool _isPressed1 = true;
   List<Event> events29April = [
     Event(
       title: "Event 1",
-      posterUrl: "https://picsum.photos/200",
+      posterUrl: "assets/posters/hcka.png",
+      time: "10:00 AM - 12:00 PM",
+      venue: "Auditorium",
+      description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    ),
+    Event(
+      title: "Event 2",
+      posterUrl: "assets/posters/codewar.png",
+      time: "1:00 PM - 3:00 PM",
+      venue: "Gymnasium",
+      description:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ),
+    Event(
+      title: "Event 3",
+      posterUrl: "assets/posters/techq.png",
+      time: "4:00 PM - 6:00 PM",
+      venue: "Football Ground",
+      description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    ),
+    Event(
+      title: "Event 1",
+      posterUrl: "assets/favicon.png",
       time: "10:00 AM - 12:00 PM",
       venue: "Auditorium",
       description:
@@ -40,6 +64,14 @@ class _MyAppState extends State<MyApp> {
       venue: "Football Ground",
       description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    ),
+    Event(
+      title: "Event 6",
+      posterUrl: "https://picsum.photos/205",
+      time: "5:00 PM - 7:00 PM",
+      venue: "Cricket Ground",
+      description:
+      "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
     ),
   ];
 
@@ -68,6 +100,38 @@ class _MyAppState extends State<MyApp> {
       description:
       "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
     ),
+    Event(
+      title: "Event 1",
+      posterUrl: "https://picsum.photos/200",
+      time: "10:00 AM - 12:00 PM",
+      venue: "Auditorium",
+      description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    ),
+    Event(
+      title: "Event 6",
+      posterUrl: "https://picsum.photos/205",
+      time: "5:00 PM - 7:00 PM",
+      venue: "Cricket Ground",
+      description:
+      "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+    ),
+    Event(
+      title: "Event 2",
+      posterUrl: "https://picsum.photos/201",
+      time: "1:00 PM - 3:00 PM",
+      venue: "Gymnasium",
+      description:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ),
+    Event(
+      title: "Event 3",
+      posterUrl: "https://picsum.photos/202",
+      time: "4:00 PM - 6:00 PM",
+      venue: "Football Ground",
+      description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    ),
   ];
   @override
   Widget build(BuildContext context) {
@@ -82,11 +146,11 @@ class _MyAppState extends State<MyApp> {
             return Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage('assets/bg.png',),
+                    image: AssetImage('assets/bgg1.gif',),
                   fit: BoxFit.cover,
                 ),
               ),
-              child: Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 80.0),
+              child: Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 70.0),
               child:
             Stack(
               children: [
@@ -113,7 +177,7 @@ class _MyAppState extends State<MyApp> {
                                 borderRadius: BorderRadius.circular(18.0),
                               ),
                             ),
-                            backgroundColor: MaterialStateProperty.all<Color>(_isPressed ? Colors.green : Colors.blue,),
+                            backgroundColor: MaterialStateProperty.all<Color>(_isPressed ? Colors.purpleAccent : Colors.blue,),
                           ),
                           child: const Text("29 April"),
                         ),
@@ -143,7 +207,7 @@ class _MyAppState extends State<MyApp> {
                                 borderRadius: BorderRadius.circular(18.0),
                               ),
                             ),
-                            backgroundColor: MaterialStateProperty.all<Color>(_isPressed1 ? Colors.green : Colors.blue,),
+                            backgroundColor: MaterialStateProperty.all<Color>(_isPressed1 ? Colors.purpleAccent : Colors.blue,),
                           ),
                           child: const Text("30 April"),
                         ),
@@ -165,63 +229,82 @@ class _MyAppState extends State<MyApp> {
                           return Card(
                             elevation: 5.0,
 
-                            color: Colors.white,
+                            color: Colors.black12,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
+                              borderRadius: BorderRadius.circular(50.0),
                               side: const BorderSide(
                                 color: Colors.purple,
                                 width: 2.0,
                               ),
                             ),
                             child: ExpansionTile(
-                              tilePadding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
+                              tilePadding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
                               title: ListTile(
-                                title: Text(event.title),
-                                subtitle: Text(event.time),
+                                title: Text(event.title,style: TextStyle(color: Colors.white),),
+                                subtitle: Text(event.time,style: TextStyle(color: Colors.white),),
                                 leading: CircleAvatar(
-                                  backgroundImage: NetworkImage(event.posterUrl),
+                                  backgroundImage: AssetImage(event.posterUrl),
                                 ),
                               ),
                               children: <Widget>[
 
                                 Padding(
-                                  padding: const EdgeInsets.all(10.0),
+                                  padding: const EdgeInsets.all(15.0),
                                   child: Column(
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      Image.network(
+                                      Image.asset(
                                         event.posterUrl,
                                         width: double.infinity,
-                                        height: 200.0,
-                                        fit: BoxFit.cover,
+                                        height: 350.0,
+                                        fit: BoxFit.fitWidth,
                                       ),
                                       const SizedBox(height: 20.0),
-                                      Text(event.title, style: Theme.of(context).textTheme.titleLarge),
+                                      Text(
+                                        event.title,
+                                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                                          color: Colors.white,
+                                        ),
+                                      ),
                                       const SizedBox(height: 10.0),
                                       Row(
                                         children: [
-                                          Icon(Icons.schedule, size: 16.0, color: Colors.grey[700]),
+                                          Icon(Icons.schedule, size: 16.0, color: Colors.purple),
                                           const SizedBox(width: 4.0),
-                                          Text(event.time, style: Theme.of(context).textTheme.titleMedium),
+                                          Text(
+                                            event.title,
+                                            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                              color: Colors.white,
+                                            ),
+                                          ),
                                         ],
                                       ),
                                       const SizedBox(height: 4.0),
                                       Row(
                                         children: [
-                                          Icon(Icons.place, size: 16.0, color: Colors.grey[700]),
+                                          Icon(Icons.place, size: 16.0, color: Colors.purple),
                                           const SizedBox(width: 4.0),
-                                          Text(event.venue, style: Theme.of(context).textTheme.titleMedium),
+                                          Text(
+                                            event.title,
+                                            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                              color: Colors.white,
+                                            ),
+                                          ),
                                         ],
                                       ),
                                       const SizedBox(height: 20.0),
-                                      Text(event.description),
+                                      Text(event.description,style: TextStyle(color: Colors.white,),),
                                       const Padding(padding: EdgeInsets.all(5.0)),
 
                                     ],
+
                                   ),
+
                                 ),
                               ],
+
                             ),
+
                           );
 
 
@@ -229,7 +312,7 @@ class _MyAppState extends State<MyApp> {
                       ),
 
                     ),
-
+                    Padding(padding: EdgeInsets.all(30.0)),
                   ],
                 ),
               ],
